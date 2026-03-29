60b249237d0296a79781c277548135c1831764db
ykrtsx-x <nichadianova@gmail.com>
2026-03-30 01:00:19 +0300

    7

---
+++ 05_lesson/lesson05_task1.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome(
+    service=ChromeService(ChromeDriverManager().install())
+)
+
+driver.get("http://uitestingplayground.com/classattr")
+
+wait = WebDriverWait(driver, 10)
+
+button = wait.until(
+    EC.visibility_of_element_located((By.CLASS_NAME, "btn-primary"))
+)
+button.click()
